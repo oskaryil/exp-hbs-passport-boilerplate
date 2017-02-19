a82a937b2b24a8b4fd1da008212d7b97c94a6b46
Oskar Yildiz <yildizoskar@gmail.com>
2017-02-19 14:43:36 +0100

    added more gulp tasks for minifying

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 gulp.task('sass', function() {
     gulp.src('./public/assets/sass/*.scss')
@@ -7,7 +12,26 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('./public/assets/css/'));
 });
 
+gulp.task('minify-css', function() {
+  return gulp.src('./public/assets/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./public/assets/css/'));
+});
+
+// gulp.task('build', function() {
+//   return gulp.src('./public/assets/css/*.css')
+//     .pipe(concat('build.css'))
+//     .pipe(gulp.dest('./public/assets/css/'));
+// });
+
 //Watch task
-gulp.task('default', function() {
+gulp.task('watch', function() {
   gulp.watch('./public/assets/sass/*.scss', ['sass']);
+  gulp.watch('./public/assets/css/*.css', ['minify-css']);
+  // gulp.watch('./public/assets/css/*.css', ['build']);
 });
